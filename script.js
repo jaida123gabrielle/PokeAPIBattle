6c122256018205bc29ce6c500047043071e5a23b
jaida <jaidao@csedge.org>
2019-05-04 15:47:19 -0700

    project

--- script.js
+++ script.js
@@ -2,17 +2,26 @@ $(document).ready(function(){
 
 
 //starter function to pull a random pokemon from the PokeAPI
+
+
+
   function getRandomPokemonLeft() {
     let randomNumber = Math.floor(Math.random() * 800) + 1;
     $.get(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`).then(function(pokeData){
       console.log("Left Pokemon Data: ", pokeData);
+      pokeData['name'];
+      console.log(pokeData['name']);
+      $("#spaceforname1").append(pokeData['name']);
     });
+
+
   }
 
 
 
 //all code here
 
+$("#leftButton").click(getRandomPokemonLeft);
 
 
 
