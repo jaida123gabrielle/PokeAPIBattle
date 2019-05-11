80702760d1da310a960c0fce346a18ece86cea83
jaida <jaidao@csedge.org>
2019-05-11 15:29:11 -0700

    oopppp

--- script.js
+++ script.js
@@ -9,19 +9,61 @@ $(document).ready(function(){
     let randomNumber = Math.floor(Math.random() * 800) + 1;
     $.get(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`).then(function(pokeData){
       console.log("Left Pokemon Data: ", pokeData);
-      pokeData['name'];
+        pokeData['name'];
       console.log(pokeData['name']);
-      $("#spaceforname1").append(pokeData['name']);
-    });
+      console.log("Left Pokemon Data: ", pokeData);
+       pokeData['sprites']['front_default'];
+      console.log(pokeData['sprites']['front_default'])
+       $("#spaceforname1").append(pokeData['name']);
 
 
-  }
+
+    let leftPokemonDisplay =  `
+      <img src="${pokeData['sprites']['front_default']}">
+      `
+      console.log(leftPokemonDisplay);
+
+      $("#left").append(leftPokemonDisplay);
+
+});
+
+}
+
+$("#leftButton").click(getRandomPokemonLeft);
 
 
 
 //all code here
 
-$("#leftButton").click(getRandomPokemonLeft);
+
+
+function getRandomPokemonRight() {
+  let randomNumber = Math.floor(Math.random() * 800) + 1;
+  $.get(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`).then(function(pokeData){
+    console.log("Right Pokemon Data: ", pokeData);
+    pokeData['name'];
+    console.log(pokeData['name']);
+    console.log("Right Pokemon Data: ", pokeData);
+      pokeData['sprites']['front_default'];
+    console.log(pokeData['sprites']['front_default'])
+    $("#spaceforname2").append(pokeData['name']);
+
+
+    let rightPokemonDisplay =  `
+    <img src="${pokeData['sprites']['front_default']}">
+    `
+  $("#rightPic").append();
+});
+
+
+}
+
+$("#rightButton").click(getRandomPokemonRight);
+
+//all code here
+
+
+
 
 
 
